826a76e4655cd22845da4237253155b2a5331620
cotete <feliperibeirots@gmail.com>
2024-09-30 11:16:03 -0300

    feito css da pagina do darwin

---
+++ src/app/darwin/page.tsx
@@ -0,0 +1,30 @@
+
+
+const Darwin = ()=>{
+
+    return(
+        <div className="p-10">
+            <h1 className="font-bold text-4xl mb-10">Teoria da Evolução de Darwin</h1>
+            <section className="h-72">
+                <div className="w-1/2">
+                    <h2 className="font-bold text-2xl mb-5">Teoria da Evolução</h2>
+                    <p>A teoria da evolução de Charles Darwin, proposta no século XIX, revolucionou a biologia e nossa compreensão da vida na Terra. Darwin argumentou que as espécies não são imutáveis, mas sim sujeitas a um processo contínuo de mudança ao longo do tempo. Seu conceito central é a seleção natural, que explica como as características favoráveis se tornam mais comuns em uma população ao longo de gerações. Essas características são heranças que aumentam a chance de sobrevivência e reprodução dos indivíduos.</p>
+                </div>
+                <div>
+                    {/*Colocar imagem*/}
+                </div>
+            </section>
+            <section className="h-72">
+                <div className="w-1/2">
+                    <h2 className="font-bold text-2xl mb-5">Seleção Natural</h2>
+                    <p>A seleção natural ocorre quando há variações entre os indivíduos de uma espécie, e essas variações impactam sua aptidão para sobreviver em um ambiente específico. Por exemplo, em um ambiente onde a comida é escassa, indivíduos que possuem habilidades superiores de forrageamento podem sobreviver e reproduzir-se mais eficazmente. Com o tempo, essas características vantajosas se tornam predominantes na população. Esse processo gradual resulta na adaptação das espécies ao seu ambiente, levando à diversidade biológica que observamos hoje.</p>
+                </div>
+                <div>
+                    {/*Colocar imagem*/}
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Darwin
