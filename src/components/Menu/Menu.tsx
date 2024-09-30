c3a715a069d4154918f4195e04eb711b40916095
gustavodscruz <gustavodiasdsc@gmail.com>
2024-09-30 11:41:02 -0300

    delatando readme e cirando route dinamico para imagem

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link"
 
-const Menu = ()=>{
+const Menu = () => {
 
-    return(
+    return (
         <nav className="flex w-1/2 flex-row justify-around">
             <Link href={"/"}></Link>
             <Link href={"/catastrofismo"}>Pagina Principal</Link>
             <Link href={"/darwin"}>Teoria Da Evolução de Darwin</Link>
-            <Link href={"/sitema"}>Sistema Solar</Link>
+            <Link href={"/sistema"}>Sistema Solar</Link>
             <Link href={"/calculo"}>Calculos</Link>
         </nav>
     )
