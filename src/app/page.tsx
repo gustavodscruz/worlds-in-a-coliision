97bd4a2be90ecb994f18f4308ad80dee8aa27e2e
cotete <feliperibeirots@gmail.com>
2024-09-30 11:59:07 -0300

    Adicionada a imagem do immanuel na pagina principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,34 @@
+import Image from "next/image"
 
 
-const Page = ()=>{
+const Page = () => {
 
-  return(
+  return (
     <div className="flex flex-col gap-7 p-10">
       <section>
         <h1 className="font-bold text-4xl mb-10">World in Collision</h1>
         <p>Worlds in Collision é um livro de Immanuel Velikovsky publicado em 1950. O livro postula que, por volta do século 15 aC, o planeta Vênus foi ejetado de Júpiter como um cometa ou objeto semelhante a um cometa e passou perto da Terra (uma colisão real não é mencionada). O objeto supostamente mudou a órbita e o eixo da Terra, causando inúmeras catástrofes que são mencionadas nas primeiras mitologias e religiões de todo o mundo. O livro foi fortemente criticado como uma obra de pseudociência e catastrofismo, e muitas de suas alegações são completamente rejeitadas pela comunidade científica estabelecida, pois não são apoiadas por nenhuma evidência disponível.</p>
       </section>
-      <section>
-        <h2 className="font-bold text-2xl mb-5">Ideias Principais</h2>
-          <div className="flex flex-col gap-5">
-            <p>Worlds in Collision é um livro de guerras na esfera celeste que ocorreram em tempos históricos. Nessas guerras, o planeta Terra também participou. [...] A história histórico-cosmológica deste livro é baseada na evidência de textos históricos de muitos povos ao redor do globo, na literatura clássica, em épicos das raças do norte, em livros sagrados dos povos do Oriente e do Ocidente, em tradições e folclore de povos primitivos, em antigas inscrições e mapas astronômicos, em achados arqueológicos,  e também em material geológico e paleontológico.</p>
-            <p>O livro propõe que Vênus se formou dentro de Júpiter e que, por volta do século 15 aC, foi ejetado de Júpiter como um cometa ou objeto semelhante a um cometa e posteriormente passou perto da Terra, embora uma colisão real com a Terra não seja mencionada. Ao fazer isso, mudou a órbita e a inclinação axial da Terra, causando inúmeras catástrofes que foram identificadas nas primeiras mitologias e tradições religiosas das civilizações humanas em todo o mundo. Cinquenta e dois anos depois, ele novamente se aproximou, interrompendo a rotação da Terra por um tempo e causando mais catástrofes. Então, nos séculos 8 e 7 aC, Marte (ele próprio deslocado por Vênus) fez aproximações da Terra; Este incidente causou uma nova rodada de distúrbios e desastres. Depois disso, a atual "ordem celestial" foi estabelecida. Os cursos dos planetas se estabilizaram ao longo dos séculos e Vênus gradualmente se tornou um planeta "normal".</p>
-        </div> 
+      <section className="flex flex-row">
+        <div className="flex flex-row justify-between">
+          <div className="w-1/2">
+            <h2 className="font-bold text-2xl mb-5">Ideias Principais</h2>
+            <div className="flex flex-col gap-5">
+              <p>Worlds in Collision é um livro de guerras na esfera celeste que ocorreram em tempos históricos. Nessas guerras, o planeta Terra também participou. [...] A história histórico-cosmológica deste livro é baseada na evidência de textos históricos de muitos povos ao redor do globo, na literatura clássica, em épicos das raças do norte, em livros sagrados dos povos do Oriente e do Ocidente, em tradições e folclore de povos primitivos, em antigas inscrições e mapas astronômicos, em achados arqueológicos,  e também em material geológico e paleontológico.</p>
+              <p>O livro propõe que Vênus se formou dentro de Júpiter e que, por volta do século 15 aC, foi ejetado de Júpiter como um cometa ou objeto semelhante a um cometa e posteriormente passou perto da Terra, embora uma colisão real com a Terra não seja mencionada. Ao fazer isso, mudou a órbita e a inclinação axial da Terra, causando inúmeras catástrofes que foram identificadas nas primeiras mitologias e tradições religiosas das civilizações humanas em todo o mundo. Cinquenta e dois anos depois, ele novamente se aproximou, interrompendo a rotação da Terra por um tempo e causando mais catástrofes. Então, nos séculos 8 e 7 aC, Marte (ele próprio deslocado por Vênus) fez aproximações da Terra; Este incidente causou uma nova rodada de distúrbios e desastres. Depois disso, a atual "ordem celestial" foi estabelecida. Os cursos dos planetas se estabilizaram ao longo dos séculos e Vênus gradualmente se tornou um planeta "normal".</p>
+            </div>
+          </div>
+          <div>
+            <Image
+              src="/img/Immanuel_Velikovsky.jpg"
+              width={500}
+              height={500}
+              alt="Júpiter"
+            />
+          </div>
+        </div>
       </section>
-      </div>
+    </div>
   )
 }
 
