e66794662862a22da8e1ad8e911000ffe6e1d7bc
enricodelguerra <ricodelguerra@gmail.com>
2024-09-30 11:13:55 -0300

    estrutura html página de erro 404 pronta

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,21 @@
-import React from "react"
+import Image from "next/image"
+
+
 
 export default function NotFound() {
     return (
-        <div>
-            <h1>Not Found - 404</h1>
-            <h2>Página não encontrada</h2>
+        <div className="flex flex-col justify-center items-center pb-3">
+            <h1 className="text-7xl">Not Found - 404</h1>
+            <figure>
+                <Image
+                    src="/img/JupiterNotFound.jpg"
+                    width={500}
+                    height={500}
+                    alt="Júpiter"
+                />
+                <figcaption>Júpiter</figcaption>
+            </figure>
+            <h2 className="text-5xl">Página não encontrada</h2>
         </div>
     )
 }
