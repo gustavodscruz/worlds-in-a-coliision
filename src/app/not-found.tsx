9a1e1143aa28c628d96a8269e3a7876e4a849aab
enricodelguerra <ricodelguerra@gmail.com>
2024-09-30 11:18:47 -0300

    tailwind iniciado na página de erro 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 
 export default function NotFound() {
     return (
-        <div className="flex flex-col justify-center items-center pb-3">
+        <div className="flex flex-col justify-center items-center pb-3 gap-10 h-full">
             <h1 className="text-7xl">Not Found - 404</h1>
             <figure>
                 <Image
