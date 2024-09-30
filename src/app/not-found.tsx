f8b73fa20051825ad942ea0a30d173f5a4876ccc
enricodelguerra <ricodelguerra@gmail.com>
2024-09-30 10:36:29 -0300

    Texto adicionado a página not found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function NotFound() {
+    return (
+        <div>
+            <h1>Not Found - 404</h1>
+            <h2>Página não encontrada</h2>
+        </div>
+    )
+}
