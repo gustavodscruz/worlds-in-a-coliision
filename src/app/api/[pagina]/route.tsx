f6b81be14f720aaa1ab9f6bec9cc472c4cd5e809
gustavodscruz <gustavodiasdsc@gmail.com>
2024-09-30 15:55:55 -0300

    env no projeto

--- src/app/api/[pagina]/route.tsx
+++ src/app/api/[pagina]/route.tsx
@@ -5,19 +5,21 @@ import { NextResponse } from "next/server";
 
 export async function GET(request:Request,{params}:{params:{pagina:string}}){
     const file = await fs.readFile(process.cwd() + '/src/data/base.json','utf-8');
-    const produtos = JSON.parse(file);    return NextResponse.json(produtos);
+    const produtos = JSON.parse(file);
+    const API_KEY = process.env.NASA_API_KEY;    
+    return NextResponse.json(produtos);
     let response;
     switch(params.pagina){
         case 'sistema':
-            response = await fetch('https://api.nasa.gov/planetary/apod?api_key=ekeoDfiuYCcLw22QJWlVFLT4iOkzgiykmhaJhHNo');
+            response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`);
         case 'darwin':
-            response = await fetch('https://api.nasa.gov/planetary/apod?api_key=ekeoDfiuYCcLw22QJWlVFLT4iOkzgiykmhaJhHNo');
+            response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`);
         case 'catastrofismo':
-            response = await fetch('https://api.nasa.gov/planetary/apod?api_key=ekeoDfiuYCcLw22QJWlVFLT4iOkzgiykmhaJhHNo');
+            response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`);
         case 'calculo':
-            response = await fetch('https://api.nasa.gov/planetary/apod?api_key=ekeoDfiuYCcLw22QJWlVFLT4iOkzgiykmhaJhHNo');    
+            response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`);
         default:
-            response = await fetch('https://api.nasa.gov/planetary/apod?api_key=ekeoDfiuYCcLw22QJWlVFLT4iOkzgiykmhaJhHNo');
+            response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`);
     }
     const data = await response.json()
     return NextResponse.json(data);
