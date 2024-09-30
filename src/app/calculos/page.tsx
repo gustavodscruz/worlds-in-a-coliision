1754ac8818e737d89f66b5c8a0cb9f9e5c88a351
enricodelguerra <ricodelguerra@gmail.com>
2024-09-30 11:55:25 -0300

    Estruturahtml calculos feita e estilização de fontes iniciada

---
+++ src/app/calculos/page.tsx
@@ -0,0 +1,25 @@
+
+export default function Calculos() {
+    return (
+        <div className="flex gap-5 flex-col">
+            <div className="flex flex-col gap-2">
+                <h1 className="text-5xl">Trajetórias Planetárias:</h1>
+                <p>Velikovsky tenta calcular as órbitas de Vênus e Marte, argumentando que suas passagens próximas à Terra causaram eventos cataclísmicos. Ele propõe que esses planetas não seguiram suas órbitas atuais, mas passaram por um processo de mudança dramática.</p>
+            </div>
+            <div>
+                <h1 className="text-5xl">Forças Gravitacionais:</h1>
+                <p>Ele discute os efeitos da gravidade nas interações entre os planetas e a Terra, sugerindo que a atração gravitacional poderia ter causado catástrofes, como inundações e terremotos.</p>
+            </div>
+            <div>
+                <h1 className="text-5xl">Mitologia e Cronologia:</h1>
+                <p>Velikovsky relaciona eventos mitológicos a fenômenos astronômicos, tentando calcular datas para eventos históricos significativos, como as pragas do Egito ou a queda de civilizações.</p>
+            </div>
+            <div>
+                <h1 className="text-5xl">Mudanças Climáticas:</h1>
+                <p>Ele sugere que as interações planetárias poderiam ter causado mudanças climáticas drásticas, influenciando a história humana.</p>
+            </div>
+
+        </div>
+
+    )
+}
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link"
 
-const Menu = ()=>{
+const Menu = () => {
 
-    return(
+    return (
         <nav className="flex w-1/2 flex-row justify-around">
             <Link href={"/"}>Pagina Principal</Link>
             <Link href={"/catastrofismo"}>Catastrofismo</Link>
             <Link href={"/darwin"}>Teoria Da Evolução de Darwin</Link>
             <Link href={"/sistema"}>Sistema Solar</Link>
-            <Link href={"/calculo"}>Calculos</Link>
+            <Link href={"/calculos"}>Calculos</Link>
         </nav>
     )
 }
